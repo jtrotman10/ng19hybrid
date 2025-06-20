467ea5c2890eb3c9ded02e809042ec2f02b458fa
Jeff Trotman <jtrotman@circlebox.com>
2025-06-20 13:41:22 -0500

    CSR and SSR

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent) },
+  { path: 'one', loadComponent: () => import('./one/one.component').then(m => m.OneComponent) },
+  { path: 'two', loadComponent: () => import('./two/two.component').then(m => m.TwoComponent) },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
---
+++ src/app/two/two.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-two',
+  imports: [],
+  templateUrl: './two.component.html',
+  styleUrl: './two.component.css'
+})
+export class TwoComponent {
+
+}
